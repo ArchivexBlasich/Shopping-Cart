47fb1ba9768fe760603912d96e3d697a2655ee71
ArchivexBlasich <fblasich0@gmail.com>
2025-09-25 23:04:09 -0300

    feat: full product view page style

--- src/pages/Dashboard/components/ProductFullView.tsx
+++ src/pages/Dashboard/components/ProductFullView.tsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router";
 import { PublicRoutes } from "../../../models/routes";
 import { printRateWithStars } from "../utils/showProductStars";
 import AddCartButton from "./AddCartButton";
+import styles from "../styles/ProductFullView.module.css"
 
 export function ProductFullView() {
   const { id } = useParams();
@@ -14,30 +15,31 @@ export function ProductFullView() {
   if (index === -1) return <NotFound />;
 
   const product = products[index];
+  console.log(product.rating)
 
   return (
     <main className="container">
-      <nav>
+      <nav className={`${styles.nav}`}>
         <Link to={`/${PublicRoutes.SHOP}`}>← Back to Shop</Link>
       </nav>
 
-      <article>
-        <header>
-          <span>{product.category}</span>
+      <article className={`${styles.article}`}>
+        <header className={`${styles.header}`}>
           <h1>{product.title}</h1>
         </header>
 
-        <figure>
+        <figure className={`${styles.figure}`}>
           <img src={product.image} alt={product.title} />
         </figure>
 
-        <section>
+        <section className={`${styles.section}`}>
           <p>{product.description}</p>
         </section>
 
-        <aside>
-          <p>${product.price}</p>
+        <aside className={`${styles.aside}`}>
+          <p>Price: ${product.price}</p>
           <div>
+            <span>Category: {product.category}</span>
             <span>Rating: {printRateWithStars(product.rating)}</span>
           </div>
           <AddCartButton id={product.id} addToCartHandler={productHandlers.handleAddToCart}/>
--- src/pages/Dashboard/utils/showProductStars.ts
+++ src/pages/Dashboard/utils/showProductStars.ts
@@ -1,3 +1,4 @@
 export const printRateWithStars = (starts: number) => {
+  starts = Math.round(starts);
   return `${"★".repeat(starts)}${"☆".repeat(5 - starts)}`;
 };
